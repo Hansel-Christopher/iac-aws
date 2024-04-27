16701be77923176dd6b4de2fa445b2a087d38929
hanselchristopher <chris23han@gmail.com>
2024-04-27 09:38:58 +0200

    fix summary

--- environments/prod/eks/main.tf
+++ environments/prod/eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
   name                  = "prod-eks"
   enable_eks_monitoring = true
   ng_min_size           = 1
-  ng_desired_size       = 1
+  ng_desired_size       = 2
   ng_max_size           = 4
   instance_types        = ["m6.large", "r6.large"]
 }
