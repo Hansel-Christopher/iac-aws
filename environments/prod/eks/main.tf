51da99f28e46da14e98036aa38495d8f61d63a09
hanselchristopher <31442893+Hansel-Christopher@users.noreply.github.com>
2024-04-29 09:48:32 +0530

    Change nodegroup instance type

--- environments/prod/eks/main.tf
+++ environments/prod/eks/main.tf
@@ -5,5 +5,5 @@ module "eks" {
   ng_min_size           = 1
   ng_desired_size       = 1
   ng_max_size           = 3
-  instance_types        = ["t3.large"]
+  instance_types        = ["r6.large"]
 }
